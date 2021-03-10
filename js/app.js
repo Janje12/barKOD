7a412fff150a08c1b2067e085ff24dc4ca7b1054
Janje12 <59922311+Janje12@users.noreply.github.com>
2021-03-10 15:35:53 +0100

    Responsivness fixed, added footer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 particlesJS('particles-js', {
     'particles': {
-        'number': {'value': 80, 'density': {'enable': true, 'value_area': 800}},
+        'number': {'value': 50, 'density': {'enable': true, 'value_area': 800}},
         'color': {'value': '#ffffff'},
         'shape': {
             'type': 'circle',
             'stroke': {'width': 0, 'color': '#000000'},
-            'polygon': {'nb_sides': 5},
+            'polygon': {'nb_sides': 7},
             'image': {'src': 'img/github.svg', 'width': 100, 'height': 100}
         },
         'opacity': {
@@ -14,7 +14,7 @@ particlesJS('particles-js', {
             'anim': {'enable': false, 'speed': 1, 'opacity_min': 0.1, 'sync': false}
         },
         'size': {
-            'value': 3,
+            'value': 4,
             'random': true,
             'anim': {'enable': false, 'speed': 40, 'size_min': 0.1, 'sync': false}
         },
@@ -33,7 +33,7 @@ particlesJS('particles-js', {
     'interactivity': {
         'detect_on': 'canvas',
         'events': {
-            'onhover': {'enable': true, 'mode': 'repulse'},
+            'onhover': {'enable': false, 'mode': 'repulse'},
             'onclick': {'enable': true, 'mode': 'push'},
             'resize': true
         },
